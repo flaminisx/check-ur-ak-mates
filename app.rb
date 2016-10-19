7f8af4e9eb5ed1e0d6f1286b5971cb362e041224
Igor Onischenko <optimum.flaminis@gmail.com>
2016-10-19 14:32:46 +0300

    some layout added

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra/base'
+require_relative 'lib/variant_checker'
 
 class MyApp < Sinatra::Base
 	set :haml, :format => :html5
 	set :views, File.join(Dir.pwd, 'views')
 	set :public_folder, Proc.new { File.join(Dir.pwd, "public") }
+
 	get '/' do
-		File.read('index.haml')
+		haml :index, locals: { students: nil }
+	end
+
+	post '/' do
+		students = VariantChecker.new(params[:number], params[:count]).check
+		puts VariantChecker
+		haml :index, locals: { students: students }
 	end
 
 end
